a175b6236a2cdc4fec3751369aa5e8ad985224e5
leisyan <you@example.com>
2024-03-21 13:59:27 +0500

    using store

---
+++ interno/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <MainPage></MainPage>
+
+</template>
+
+<script>
+import MainPage from "./pages/MainPage.vue";
+
+export default {
+  components: { MainPage },
+}
+</script>
+
+<style lang="scss">
+
+</style>
